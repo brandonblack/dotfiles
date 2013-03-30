f2703a03bc7c4625bae839288f51291bd6ec61c5
Brandon Black <brandonmblack@gmail.com>
2013-03-29 17:19:16 -0700

    new function: repeat

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ curl get.pow.cx | sh
 ./shell/install.sh
 
 # Copy to home folder
-for file in .{zshrc,aliases,exports,functions,gitconfig,gemrc,pryrc,railsrc,ruby-version,irbrc}; do
+for file in .{zshrc,aliases,exports,functions,gitconfig,gemrc,pryrc,railsrc,ruby-version,irbrc,jrubyrc}; do
   cp "$file" ~/
 done
 unset file
