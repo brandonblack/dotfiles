4a87d97af2f872ef752a21aa5f10935181770498
Brandon Black <brandonmblack@gmail.com>
2013-06-27 11:16:52 -0700

    removing pow and changing global gemset

--- install.sh
+++ install.sh
@@ -13,19 +13,6 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 # Run the RVM install
 ./rvm/install.sh
 
-# Install Pow (http://pow.cx)
-curl get.pow.cx | sh
-
-# Pow Notes: Go open node_modules/nack/lib/nack/server.rb inside
-# ~/Library/Application Support/Pow/Current and replace
-# sock.close_read with:
-#
-# begin
-#   sock.close_read
-# rescue IOError
-#   raise unless RUBY_ENGINE == 'jruby'
-# end
-
 # Switch to z-shell and install oh-my-zsh
 ./shell/install.sh
 
