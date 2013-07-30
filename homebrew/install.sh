aba4418537f84d24b61d74206a8ab1a44710e4ea
Brandon Black <brandonmblack@gmail.com>
2013-07-29 18:10:24 -0700

    adding some new brews to my list of defaults

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -6,15 +6,11 @@ ruby <(curl -fsSkL raw.github.com/mxcl/homebrew/go)
 brew install findutils
 brew install coreutils
 echo "Don’t forget to add $(brew --prefix coreutils)/libexec/gnubin to \$PATH."
-brew install ack
 
 brew tap homebrew/dupes
 brew install apple-gcc42
 sudo ln -s /usr/local/bin/gcc-4.2 /usr/bin/gcc-4.2
 
 brew install homebrew/dupes/grep
-brew install git
-brew install node
-brew install hub
-brew install reattach-to-user-namespace
+brew install git node hub reattach-to-user-namespace tree ack
 brew cleanup
