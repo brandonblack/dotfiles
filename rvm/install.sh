a0a272742a474f31b898a28c2e4d4ac74534e719
Brandon Black <brandonmblack@gmail.com>
2013-10-03 10:44:08 -0700

    updating rvm install script and default gemsets

--- rvm/install.sh
+++ rvm/install.sh
@@ -3,9 +3,9 @@
 # RVM Install
 curl -L https://get.rvm.io | bash -s stable
 
-cp rvm/*.gems ~/.rvm/gemsets
+cp -R rvm/gemsets/* ~/.rvm/gemsets
 
-rvm install 1.8.7 --no-tk --autolibs=4
+rvm install 1.8.7 --autolibs=4
 rvm install 1.9.3 --autolibs=4
 rvm install jruby --autolibs=4
 rvm install 2.0.0 --autolibs=4
