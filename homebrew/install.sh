f5a1650363b6c38d4094ab39303eed29a7cd0726
Brandon Black <brandonmblack@gmail.com>
2013-10-30 11:55:35 -0700

    removing drip since it didn't play way under rvm/jruby

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -12,5 +12,5 @@ brew install apple-gcc42
 sudo ln -s /usr/local/bin/gcc-4.2 /usr/local/bin/gcc
 
 # Install default brew packages
-brew install git node hub reattach-to-user-namespace tree ack drip
+brew install git node hub reattach-to-user-namespace tree ack
 brew cleanup
