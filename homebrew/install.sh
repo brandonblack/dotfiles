a0aa9a73dec1bd90a6c211339f1972ebcc1dcc19
Brandon Black <brandonmblack@gmail.com>
2013-11-01 12:51:39 -0700

    adding symlnks for g++ and gcc under mavericks

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -9,7 +9,8 @@ echo "Don’t forget to add $(brew --prefix coreutils)/libexec/gnubin to \$PATH.
 
 # Install non-LLVM gcc
 brew install apple-gcc42
-sudo ln -s /usr/local/bin/gcc-4.2 /usr/local/bin/gcc
+sudo ln -s `brew --prefix apple-gcc42`/bin/gcc-4.2 /usr/bin/gcc-4.2
+sudo ln -s `brew --prefix apple-gcc42`/bin/g++-4.2 /usr/bin/gcc-4.2
 
 # Install default brew packages
 brew install git node hub reattach-to-user-namespace tree ack
