73ca0ec7d25c9751516aaed8abbcbcdc525212bb
Brandon Black <brandonmblack@gmail.com>
2017-04-07 07:44:53 -0700

    updating homebrew url

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install Homebrew
-ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # Install GNU Unix Tools
 brew install findutils coreutils
